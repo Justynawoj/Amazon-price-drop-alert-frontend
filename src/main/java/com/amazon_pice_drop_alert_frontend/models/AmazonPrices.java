d5b6c72a66197c83e59ecaf081c690f4819d5008
justynaWoj <j.wojciechowska2011@gmail.com>
2021-02-07 18:39:36 +0100

    fixed AmazonPrices, ProductInfo, ThirdPartPrices, added mapper to Service

--- src/main/java/com/amazon_pice_drop_alert_frontend/models/AmazonPrices.java
+++ src/main/java/com/amazon_pice_drop_alert_frontend/models/AmazonPrices.java
@@ -2,7 +2,58 @@ package com.amazon_pice_drop_alert_frontend.models;
 
 public class AmazonPrices {
 
-    private PriceDetailsDto currentPriceAmazon;
-    private PriceDetailsDto highestPriceAmazon;
-    private PriceDetailsDto lowestPricingAmazon;
+    private double currentPriceAmazon;
+    private String dateCurrentPrice;
+    private double highestPriceAmazon;
+    private String dateHighestPriceAmazon;
+    private double lowestPricingAmazon;
+    private String dateLowestPricingAmazon;
+
+    public double getCurrentPriceAmazon() {
+        return currentPriceAmazon;
+    }
+
+    public void setCurrentPriceAmazon(double currentPriceAmazon) {
+        this.currentPriceAmazon = currentPriceAmazon;
+    }
+
+    public String getDateCurrentPrice() {
+        return dateCurrentPrice;
+    }
+
+    public void setDateCurrentPrice(String dateCurrentPrice) {
+        this.dateCurrentPrice = dateCurrentPrice;
+    }
+
+    public double getHighestPriceAmazon() {
+        return highestPriceAmazon;
+    }
+
+    public void setHighestPriceAmazon(double highestPriceAmazon) {
+        this.highestPriceAmazon = highestPriceAmazon;
+    }
+
+    public String getDateHighestPriceAmazon() {
+        return dateHighestPriceAmazon;
+    }
+
+    public void setDateHighestPriceAmazon(String dateHighestPriceAmazon) {
+        this.dateHighestPriceAmazon = dateHighestPriceAmazon;
+    }
+
+    public double getLowestPricingAmazon() {
+        return lowestPricingAmazon;
+    }
+
+    public void setLowestPricingAmazon(double lowestPricingAmazon) {
+        this.lowestPricingAmazon = lowestPricingAmazon;
+    }
+
+    public String getDateLowestPricingAmazon() {
+        return dateLowestPricingAmazon;
+    }
+
+    public void setDateLowestPricingAmazon(String dateLowestPricingAmazon) {
+        this.dateLowestPricingAmazon = dateLowestPricingAmazon;
+    }
 }
--- src/main/java/com/amazon_pice_drop_alert_frontend/models/ProductInfo.java
+++ src/main/java/com/amazon_pice_drop_alert_frontend/models/ProductInfo.java
@@ -7,4 +7,35 @@ public class ProductInfo {
     private String createdAt;
     private String currencySymbol;
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAsin(String asin) {
+        this.asin = asin;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setCurrencySymbol(String currencySymbol) {
+        this.currencySymbol = currencySymbol;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAsin() {
+        return asin;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getCurrencySymbol() {
+        return currencySymbol;
+    }
 }
